fc2e518aa93159fc07a3c201dc0f25fa0d06c930
unknown <andre.lmm91@gmail.com>
2022-10-15 22:04:39 +0200

    adding dynamically styles with CCS module native React

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import { useState } from "react";
+import React from "react";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-// props here is coming from the FormControl down below. It is very useful for dinamically styling.
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// // props here is coming from the FormControl down below. It is very useful for dinamically styling.
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'};
-  }
- 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? 'salmon' : '#f5eff3'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #f5eff3;
-    border-color: #86637b;
-  }
-`;
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "salmon" : "#f5eff3")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #f5eff3;
+//     border-color: #86637b;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -54,17 +54,17 @@ const CourseInput = (props) => {
   // // with div
   // `` is a in-built javascript for templating. It will be treating as normal string and we can add JS inside $
   // we are adding dinamically the ccs class by using  isValid as a condition
-  // <div className={`form-control ${!isValid ? "invalid" : ""}`}></div>
-  
+  // <div className={`form_control ${!isValid ? "invalid" : ""}`}></div>
+
   // // with FormControl controlling the css output
   // <FormControl className={!isValid && 'invalid'}>
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={styles.form_control}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
