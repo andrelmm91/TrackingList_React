6b9c97898c3cc16970d1ba831b232f580289489f
unknown <andre.lmm91@gmail.com>
2022-10-15 19:59:15 +0200

    dynamically change ccs styles when Add Goal is empty

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,8 +8,8 @@ const CourseInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    if (event.target.value.trim().length > 0){
-      setIsValid(true);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true); // after starting typing, remove the red background
     }
     setEnteredValue(event.target.value);
   };
@@ -23,18 +23,13 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
+  // `` is a in-built javascript for templating. It will be treating as normal string and we can add JS inside $
+  // we are adding dinamically the ccs class by using  isValid as a condition
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? "red" : "#ccc",
-            background: !isValid ? "salmon" : "transparent",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
