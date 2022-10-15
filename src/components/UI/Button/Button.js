d1ab2aba3079591b0c8fa3e54ec65b0d80131e7e
unknown <andre.lmm91@gmail.com>
2022-10-15 21:48:27 +0200

    adding dynamically styles with CCS module native React

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,46 @@
 // import './Button.css';
+import React from 'react';
+import styles from './Button.module.css'
 
-import styled from "styled-components";
-// .button is a method from styled.
-// by using Styled package, we dont use a CSS file, but paste here.
-const Button = styled.button`
-    width: 100%
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
 
-  @media (min-width: 840px) {
-    width: auto
-  }
 
-  &:focus {
-    outline: none;
-  }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
 
-// // normally, we do this way, but we use Styled-component (above mentioned)
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+// // // this way we use Styled-component package
+// import styled from "styled-components";
+// // .button is a method from styled.
+// // by using Styled package, we dont use a CSS file, but paste here.
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//   // styling for mobile
+//   @media (min-width: 640px) {
+//     width: auto;
+//   }
+//   &:focus {
+//     outline: none;
+//   }
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+// // // normally, we do this way, but we use Styled-component (above mentioned)
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
